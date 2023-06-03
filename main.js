f614edb774f82702c860490e65fe08bce76c7b71
Lauren DeLaRosa <lstoneman13@gmail.com>
2023-06-03 17:03:22 -0400

    Update star button functionality

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
 // querySelectors
-var form = document.querySelector('form');
-var titleInput = document.querySelector('#title');
-var bodyInput = document.querySelector('#body');
-var inputs = document.querySelectorAll('.input');
-var saveButton = document.querySelector('.saveBtn');
-var lowerPane = document.querySelector('.lowerPane');
+var form = document.querySelector("form");
+var titleInput = document.querySelector("#title");
+var bodyInput = document.querySelector("#body");
+var inputs = document.querySelectorAll(".input");
+var saveButton = document.querySelector(".saveBtn");
+var lowerPane = document.querySelector(".lowerPane");
 
 // eventListeners
-saveButton.addEventListener('click', function (event) {
+saveButton.addEventListener("click", function (event) {
   event.preventDefault();
   var idea = createNewIdea(titleInput.value, bodyInput.value);
   addToAllIdeas(idea);
   renderAllIdeas();
   form.reset();
+<<<<<<< Updated upstream
+=======
+  saveButton.setAttribute("disabled", "");
+>>>>>>> Stashed changes
 });
 
 for (var i = 0; i < inputs.length; i++) {
-  inputs[i].addEventListener('keyup', handleInputChange);
+  inputs[i].addEventListener("keyup", handleInputChange);
 }
 
+<<<<<<< Updated upstream
 lowerPane.addEventListener('click', handleIdeaRemoval);
+=======
+lowerPane.addEventListener("click", function (event) {
+  var button = event.target;
+  if (button.classList.contains("star-btn")) {
+    handleIdeaFavorite(event);
+  } else if (button.classList.contains("card-delete-btn")) {
+    handleIdeaRemoval(event);
+  }
+});
+>>>>>>> Stashed changes
 
 // global variables
 var allIdeas = [];
@@ -39,25 +54,34 @@ function addToAllIdeas(idea) {
 }
 
 function createCard(idea) {
-  var cardContainer = document.createElement('div');
-  cardContainer.classList.add('card');
+  var cardContainer = document.createElement("div");
+  cardContainer.classList.add("card");
   cardContainer.id = idea.id;
 
-  var cardBtnContainer = document.createElement('div');
-  cardBtnContainer.classList.add('card-btn-container');
+  var cardBtnContainer = document.createElement("div");
+  cardBtnContainer.classList.add("card-btn-container");
   cardContainer.appendChild(cardBtnContainer);
 
-  var deleteBtn = document.createElement('button');
-  deleteBtn.classList.add('card-delete-btn');
+  var deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("card-delete-btn");
   cardBtnContainer.appendChild(deleteBtn);
 
+<<<<<<< Updated upstream
   var title = document.createElement('h3');
   title.classList.add('cardTitle');
+=======
+  var starBtn = document.createElement("button");
+  starBtn.classList.add("star-btn");
+  cardBtnContainer.appendChild(starBtn);
+
+  var title = document.createElement("h3");
+  title.classList.add("cardTitle");
+>>>>>>> Stashed changes
   title.innerText = idea.title;
   cardContainer.appendChild(title);
 
-  var body = document.createElement('p');
-  body.classList.add('cardBody');
+  var body = document.createElement("p");
+  body.classList.add("cardBody");
   body.innerText = idea.body;
   cardContainer.appendChild(body);
 
@@ -65,7 +89,7 @@ function createCard(idea) {
 }
 
 function renderAllIdeas() {
-  lowerPane.innerHTML = '';
+  lowerPane.innerHTML = "";
 
   for (var i = 0; i < allIdeas.length; i++) {
     var card = createCard(allIdeas[i]);
@@ -74,17 +98,26 @@ function renderAllIdeas() {
 }
 
 function checkIfFilledOut() {
-  return titleInput.value !== '' && bodyInput.value !== '';
+  return titleInput.value !== "" && bodyInput.value !== "";
 }
 
 function updateSaveButton(isFilledOut) {
   if (isFilledOut) {
+<<<<<<< Updated upstream
       saveButton.removeAttribute('disabled');
       saveButton.classList.add('enabled');
     } else {
       saveButton.setAttribute('disabled', '');
       saveButton.classList.remove('enabled');
     }
+=======
+    saveButton.removeAttribute("disabled");
+    saveButton.classList.add("enabled");
+  } else {
+    saveButton.setAttribute("disabled", "");
+    saveButton.classList.remove("enabled");
+  }
+>>>>>>> Stashed changes
 }
 
 function handleInputChange() {
@@ -92,9 +125,8 @@ function handleInputChange() {
   updateSaveButton(isFilledOut);
 }
 
-
 function removeIdeaFromArray(event) {
-  var card = event.target.closest('.card');
+  var card = event.target.closest(".card");
 
   for (var i = 0; i < allIdeas.length; i++) {
     if (allIdeas[i].id === parseInt(card.id)) {
@@ -106,4 +138,37 @@ function removeIdeaFromArray(event) {
 function handleIdeaRemoval(event) {
   removeIdeaFromArray(event);
   renderAllIdeas();
+<<<<<<< Updated upstream
 }
+=======
+}
+
+function addToFavorites(event) {
+  var card = event.target.closest(".card");
+  for (var i = 0; i < allIdeas.length; i++) {
+    if (allIdeas[i].id === parseInt(card.id)) {
+      if (allIdeas[i].isFavorite === false) {
+        allIdeas[i].isFavorite = true;
+      } else if (allIdeas[i].isFavorite === true) {
+        allIdeas[i].isFavorite = false;
+      }
+    }
+  }
+}
+
+function markFavorited() {
+  var starButtons = document.getElementsByClassName("star-btn");
+  for (var i = 0; i < allIdeas.length; i++) {
+    if (allIdeas[i].isFavorite === true) {
+      starButtons[i].style.background = "url(./assets/star-active.svg)";
+    } else {
+      starButtons[i].style.background = "url(./assets/star.svg)";
+    }
+  }
+}
+
+function handleIdeaFavorite(event) {
+  addToFavorites(event);
+  markFavorited();
+}
+>>>>>>> Stashed changes
